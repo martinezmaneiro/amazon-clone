32be0bf6f09ba0f9cf05aba732c1aee923b8ddf1
Joaquin Martinez Maneiro <joaquinmar08@gmail.com>
2023-04-25 16:57:36 -0300

    [ADDED] header logo

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,7 +1,11 @@
+import { logo } from "../../assets/index";
+
 const Header =()=> {
     return (
-        <div>
-            Header
+        <div className='w-full bg-amazon_blue text-white px-4 py-3'>
+            <div className='px-2 h-[80%] flex items-center border border-transparent hover:border-white cursor-pointer duration-100'>
+                <img src={logo} alt='amazon logo' className='w-24 mt-2'/>
+            </div>
         </div>
     )
 };
