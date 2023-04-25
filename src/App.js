140269d21a31523084dfbcd22ac371cf021dc1c9
Joaquin Martinez Maneiro <joaquinmar08@gmail.com>
2023-04-25 17:14:47 -0300

    [ADDED] deliver box

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import Header from './components/header/Header';
 
 function App() {
   return (
-    <div className="App">
+    <div className="font-bodyFont">
       <Header/>
     </div>
   );
--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,10 +1,15 @@
 import { logo } from "../../assets/index";
+import LocationOnOutlinedIcon from '@mui/icons-material/LocationOnOutlined';
 
 const Header =()=> {
     return (
-        <div className='w-full bg-amazon_blue text-white px-4 py-3'>
-            <div className='px-2 h-[80%] flex items-center border border-transparent hover:border-white cursor-pointer duration-100'>
+        <div className='w-full bg-amazon_blue text-white px-4 py-3 flex items-center gap-4'>
+            <div className='headerHover'>
                 <img src={logo} alt='amazon logo' className='w-24 mt-2'/>
+            </div>
+            <div className='headerHover'>
+                <LocationOnOutlinedIcon/>
+                <p className='text-sm text-lightText font-light flex flex-col'>Deliver to <span className='text-sm font-semibold -mt-1 text-whiteText'>Uruguay</span></p>
             </div>
         </div>
     )
