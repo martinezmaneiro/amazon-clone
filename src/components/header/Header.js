53c50005d4ddf4d7e8d4b73821f2517aa040dbfb
Joaquin Martinez Maneiro <joaquinmar08@gmail.com>
2023-04-25 17:23:37 -0300

    [ADDED] search bar

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,5 +1,8 @@
 import { logo } from "../../assets/index";
 import LocationOnOutlinedIcon from '@mui/icons-material/LocationOnOutlined';
+import ArrowDropDownOutlinedIcon from '@mui/icons-material/ArrowDropDownOutlined';
+import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
+import SearchIcon from '@mui/icons-material/Search';
 
 const Header =()=> {
     return (
@@ -10,6 +13,11 @@ const Header =()=> {
             <div className='headerHover'>
                 <LocationOnOutlinedIcon/>
                 <p className='text-sm text-lightText font-light flex flex-col'>Deliver to <span className='text-sm font-semibold -mt-1 text-whiteText'>Uruguay</span></p>
+            </div>
+            <div className='h-10 rounded-md flex flex-grow relative'>
+                <span>All <span><ArrowDropDownOutlinedIcon/></span></span>
+                <input className='h-full text-base text-amazon_blue flex-grow outline-none border-none px-2' type='text'/>
+                <span><SearchIcon/></span>
             </div>
         </div>
     )
