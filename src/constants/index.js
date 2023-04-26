196f9d24f090355160025d5e08225a8b3ed5cf18
Joaquin Martinez Maneiro <joaquinmar08@gmail.com>
2023-04-26 08:14:08 -0300

    [ADDED] allItems to js file in constants folder

---
+++ src/constants/index.js
@@ -0,0 +1,88 @@
+const allItems = [
+    {
+    _id:'101', title: 'All Departments'
+    },
+    {
+    _id:'102', title: 'Arts & Crafts'
+    },
+    {
+    _id:'103', title: 'Automotive'
+    },
+    {
+    _id:'104', title: ' Baby'
+    },
+    {
+    _id:'105', title: 'Beauty & Personal Care'
+    },
+    {
+    _id:'106', title: 'Books'
+    },
+    {
+    _id:'107', title: 'Boys\' fashion'
+    },
+    {
+    _id:'108', title: 'Computers'
+    },
+    {
+    _id:'109', title: 'Deals'
+    },
+    {
+    _id:'110', title: 'Digital Music'
+    },
+    {
+    _id:'111', title: 'Electronics'
+    },
+    {
+    _id:'112', title: 'Girls\' Fashion'
+    },
+    {
+    _id:'113', title: 'Health & Household'
+    },
+    {
+    _id:'114', title: 'Home & Kitchen'
+    },
+    {
+    _id:'115', title: 'Industrial & Scientific'
+    },
+    {
+    _id:'116', title: 'Kindle Store'
+    },
+    {
+    _id:'117', title: 'Luggage'
+    },
+    {
+    _id:'118', title: 'Men\'s Fashion'
+    },
+    {
+    _id:'119', title: 'Movies & TV'
+    },
+    {
+    _id:'120', title: 'Music. CDs & Vinyl'
+    },
+    {
+    _id:'121', title: 'Pet Supplies'
+    },
+    {
+    _id:'122', title: 'Prime Video'
+    },
+    {
+    _id:'123', title: 'Software'
+    },
+    {
+    _id:'124', title: 'Sports & Outdoors'
+    },
+    {
+    _id:'125', title: 'Tools & Home Improvement'
+    },
+    {
+    _id:'126', title: 'Toys & Games'
+    },
+    {
+    _id:'127', title: 'Video Games'
+    },
+    {
+    _id:'128', title: 'Women\'s Fashion'
+    },
+];
+
+export default allItems;
