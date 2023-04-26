d742b38a44d124685b3aa5342b9fbeda1d2d1f81
Joaquin Martinez Maneiro <joaquinmar08@gmail.com>
2023-04-26 08:44:15 -0300

    [ADDED] header cart

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -3,7 +3,7 @@ import { logo } from '../../assets/index';
 import allItems from '../../constants/index';
 import LocationOnOutlinedIcon from '@mui/icons-material/LocationOnOutlined';
 import ArrowDropDownOutlinedIcon from '@mui/icons-material/ArrowDropDownOutlined';
-/*import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';*/
+import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
 import SearchIcon from '@mui/icons-material/Search';
 
 const Header =()=> {
@@ -49,6 +49,13 @@ const Header =()=> {
             <div className='flex flex-col items-start justify-center headerHover'>
                 <p className='text-xs text-lightText font-light'>Returns</p>
                 <p className='text-sm font-semibold -mt-1 text-whiteText'>& Orders</p>
+            </div>
+            <div className='flex items-start justify-center headerHover relative'>
+                <ShoppingCartIcon/>
+                <p className='text-xs font-semibold mt-3 text-whiteText'>
+                    Cart
+                    <span className='absolute text-sm -top-1 left-6 font-semibold p-1 h-4 bg-[#F3A847] text-amazon_blue rounded-full flex justify-center items-center'>0</span>
+                </p>
             </div>
         </div>
     )
