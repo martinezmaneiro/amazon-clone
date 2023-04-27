91d4a1062c96776f5c38fd68218d7020f8e8a2b6
Joaquin Martinez Maneiro <joaquinmar08@gmail.com>
2023-04-27 08:36:36 -0300

    [ADDED] sidebar open/close state & visuals

--- src/components/header/HeaderBottom.js
+++ src/components/header/HeaderBottom.js
@@ -1,12 +1,21 @@
-import { useState } from 'react';
+import { useState, useRef, useEffect } from 'react';
 import MenuIcon from '@mui/icons-material/Menu';
 import CloseIcon from '@mui/icons-material/Close';
 import AccountCircleIcon from '@mui/icons-material/AccountCircle';
+import { motion } from 'framer-motion';
 import SideNavContent from './SideNavContent';
 
-
 const HeaderBottom =()=> {
+    const ref = useRef();
     const [sidebar, setSidebar] = useState(false);
+    useEffect(() => {
+        document.body.addEventListener('click', (e) =>{
+            if(e.target.contains(ref.current)){
+                setSidebar(false)
+            }
+        })
+    }, [])
+
     return (
         <div className='w-full px-4 h-[36px] bg-amazon_light text-white flex items-center'>
             <ul className='flex items-center gap-2 text-sm tracking-wide'>
@@ -20,13 +29,17 @@ const HeaderBottom =()=> {
             {sidebar && (
                 <div className='w-full h-screen text-black fixed top-0 left-0 bg-amazon_blue bg-opacity-50'>
                     <div className='w-full h-full relative'>
-                        <div className='w-[350px] h-full bg-white border border-black'>
+                        <motion.div ref={ref} initial={{x:-500, opacity:0}} animate={{x:0, opacity:1}} transition={{duration:0.5}} className='w-[350px] h-full bg-white border border-black'>
                             <div className='w-full bg-amazon_light text-white py-2 px-6 flex items-center gap-4'>
                                 <AccountCircleIcon/>
                                 <h3 className='font-titleFont font-bold text-lg tracking-wide'>Hello, Sign In</h3>
                             </div>
-                            <SideNavContent/>
-                        </div>
+                            <SideNavContent title='Digital Content & Devices' one='Amazon Music' two='Kindle E--readers & Books' three='Amazon Appstore'/>
+                            <SideNavContent title='Shop By Department' one='Electronics' two='Computers' three='Smart Home'/>
+                            <SideNavContent title='Programs & Features' one='Gift Cards' two='Amazon live' three='International Shopping'/>
+                            <SideNavContent title='Help & Settings' one='Your Account' two='Customer Service' three='Contact us'/>
+                            <span onClick={() => setSidebar(false)} className='cursor-pointer absolute top-0 left-[360px] w-10 h-10 text-blackk flex items-center justify-center border bg-gray-200 hover:bg-red-500 hover:text-white duration-300'><CloseIcon/></span>
+                        </motion.div>
                     </div>
                 </div>
             )}
--- src/components/header/SideNavContent.js
+++ src/components/header/SideNavContent.js
@@ -1,22 +1,22 @@
 import KeyboardArrowRightIcon from '@mui/icons-material/KeyboardArrowRight';
 
-const SideNavContent =()=> {
+const SideNavContent =({title, one, two, three})=> {
     return (
         <div className='py-3 border-b-[1px] border-b-gray-300'>
             <h3 className='text-lg font-titleFont font-semibold mb-1 px-6'>
-                Digital Content & Devices
+                {title}
             </h3>
-            <ul>
-                <li className='flex items-center justify-between hover:bg-zinc-200 px-6 '>
-                    Amazon Music
+            <ul className='text-sm'>
+                <li className='flex items-center justify-between hover:bg-zinc-200 px-6 py-2 cursor-pointer'>
+                    {one}
                     <span><KeyboardArrowRightIcon/></span>
                 </li>
-                <li className='flex items-center justify-between hover:bg-zinc-200 px-6 '>
-                    Amazon Music
+                <li className='flex items-center justify-between hover:bg-zinc-200 px-6 py-2 cursor-pointer'>
+                    {two}
                     <span><KeyboardArrowRightIcon/></span>
                 </li>
-                <li className='flex items-center justify-between hover:bg-zinc-200 px-6 '>
-                    Amazon Music
+                <li className='flex items-center justify-between hover:bg-zinc-200 px-6 py-2 cursor-pointer'>
+                    {three}
                     <span><KeyboardArrowRightIcon/></span>
                 </li>
             </ul>
