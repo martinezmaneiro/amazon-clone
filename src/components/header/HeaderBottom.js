ba14f7bc6b7226b4fa01d971d1d37dcdeed2adc4
Joaquin Martinez Maneiro <joaquinmar08@gmail.com>
2023-04-28 06:23:20 -0300

    [ADDED] header bottom component responsive

--- src/components/header/HeaderBottom.js
+++ src/components/header/HeaderBottom.js
@@ -20,16 +20,16 @@ const HeaderBottom =()=> {
         <div className='w-full px-4 h-[36px] bg-amazon_light text-white flex items-center'>
             <ul className='flex items-center gap-2 text-sm tracking-wide'>
                 <li onClick={() => setSidebar(true)} className='headerHover flex items-center gap-1'><MenuIcon/>All</li>
-                <li className='headerHover'>Today's Deals</li>
-                <li className='headerHover'>Customer Service</li>
-                <li className='headerHover'>Gift Cards</li>
-                <li className='headerHover'>Registry</li>
-                <li className='headerHover'>Sell</li>
+                <li className='headerHover hidden md:inline-flex'>Today's Deals</li>
+                <li className='headerHover hidden md:inline-flex'>Customer Service</li>
+                <li className='headerHover hidden md:inline-flex'>Gift Cards</li>
+                <li className='headerHover hidden md:inline-flex'>Registry</li>
+                <li className='headerHover hidden md:inline-flex'>Sell</li>
             </ul>
             {sidebar && (
                 <div className='w-full h-screen text-black fixed top-0 left-0 bg-amazon_blue bg-opacity-50'>
                     <div className='w-full h-full relative'>
-                        <motion.div ref={ref} initial={{x:-500, opacity:0}} animate={{x:0, opacity:1}} transition={{duration:0.5}} className='w-[350px] h-full bg-white border border-black'>
+                        <motion.div ref={ref} initial={{x:-500, opacity:0}} animate={{x:0, opacity:1}} transition={{duration:0.5}} className='md:w-[350px] w-[100%] h-auto bg-white border border-black'>
                             <div className='w-full bg-amazon_light text-white py-2 px-6 flex items-center gap-4'>
                                 <AccountCircleIcon/>
                                 <h3 className='font-titleFont font-bold text-lg tracking-wide'>Hello, Sign In</h3>
@@ -38,7 +38,9 @@ const HeaderBottom =()=> {
                             <SideNavContent title='Shop By Department' one='Electronics' two='Computers' three='Smart Home'/>
                             <SideNavContent title='Programs & Features' one='Gift Cards' two='Amazon live' three='International Shopping'/>
                             <SideNavContent title='Help & Settings' one='Your Account' two='Customer Service' three='Contact us'/>
-                            <span onClick={() => setSidebar(false)} className='cursor-pointer absolute top-0 left-[360px] w-10 h-10 text-blackk flex items-center justify-center border bg-gray-200 hover:bg-red-500 hover:text-white duration-300'><CloseIcon/></span>
+                            <span onClick={() => setSidebar(false)} className='cursor-pointer absolute top-[2px] md:top-0 left-[70%] md:left-[360px] w-10 h-10 text-black flex items-center justify-center border bg-gray-200 hover:bg-red-500 hover:text-white duration-300'>
+                                <CloseIcon/>
+                            </span>
                         </motion.div>
                     </div>
                 </div>
