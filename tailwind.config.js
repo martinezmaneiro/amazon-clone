c1eb621da03e40422c36e38d279dd3a5111797f6
Joaquin Martinez Maneiro <joaquinmar08@gmail.com>
2023-05-02 10:16:10 -0300

    [COMMENTED] tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,11 @@
 /** @type {import('tailwindcss').Config} */
+
 module.exports = {
   content: [
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
+    //setting tailwind css custom constants
     extend: {
       maxWidth:{
         container:"1440px"
